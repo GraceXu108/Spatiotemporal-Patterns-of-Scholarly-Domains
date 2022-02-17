010c358faf3a8d743e9d03dc75fafc8646377873
Yifan_Xu <ge93yaj@mytum.de>
2022-02-17 20:01:20 +0800

    adjust config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: '/',
+    // path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     static: './dist',
